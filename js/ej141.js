803852000ac966ec746f67e5a7fffd791458d307
jorogre <jonygreco@gmail.com>
2017-08-29 21:59:51 -0300

    actualizo los ejercicios de js

--- js/ej141.js
+++ js/ej141.js
@@ -1,5 +1,5 @@
 const gatosDeViaje = [500,200,150];
-//sumar valores con map
+//sumar valores con reduce
 
 const total = gatosDeViaje.reduce(function(total, pesos){
     return total + pesos;
--- js/ej154.js
+++ js/ej154.js
@@ -7,7 +7,7 @@ const jugador = {
             if(danio < 0){
                 return 'No puedes tener energia negativa';
             }else{
-                this.energia = this.energia - danio;
+                this.energia -= danio;
                 return 'Te quedan ' + this.energia + '% de energia';
             }
         }
@@ -15,26 +15,25 @@ const jugador = {
     recuperarEnergia:  function(energiaRecuperada){
         let sumarEnergia = this.energia + energiaRecuperada;
         if(sumarEnergia <= 100){
-            this.energia = this.energia + energiaRecuperada;
+            this.energia += energiaRecuperada;
             return 'Enegia recargada, ahora tienes: ' + this.energia+'%';
         }else{
             return 'la energia no puede pasar de los 100, ingrese un numero menor';
         }
     },
-    perderVida:  function(quitarVida){
-        if(quitarVida < 0 || quitarVida > 1){
-            return 'no puedes quitar una vida negativa o quitar mas de 2 vidas';
-        }else{
-            this.vidas = this.vidas - quitarVida;
-            return 'Te quedan: ' + this.vidas + ' vidas';
+    perderVida:  function(){
+        if(this.vidas < 0){
+            this.vidas = 0;
+            return 'ya no tienes vidas';
         }
+        return 'te quedan: '+this.vidas-- +' vidas';
     },
     recuperarVida:  function(agregarVida){
         let agregarVidas = this.vidas + agregarVida;
         if(agregarVida < 0 || agregarVida > 99 || agregarVidas > 99){
             return 'No puedes agregar una vida negativa o agregar mas de 99 vidas';
         }else{
-            this.vidas = this.vidas + agregarVida;
+            this.vidas += agregarVida;
             return 'Recuperaste: ' + agregarVida + ' vida(s), ahora tienes: ' + this.vidas + ' vidas';
         }
     }
@@ -46,7 +45,9 @@ console.log(jugador.perderEnergia(40));
 console.log(`El jugador ${jugador.nombre} tiene ${jugador.energia}% de energía y ${jugador.vidas} vidas`);
 console.log(jugador.recuperarEnergia(0));
 console.log(`El jugador ${jugador.nombre} tiene ${jugador.energia}% de energía y ${jugador.vidas} vidas`);
-console.log(jugador.perderVida(6));
+console.log(jugador.perderVida());
 console.log(`El jugador ${jugador.nombre} tiene ${jugador.energia}% de energía y ${jugador.vidas} vidas`);
 console.log(jugador.recuperarVida(10));
+console.log(`El jugador ${jugador.nombre} tiene ${jugador.energia}% de energía y ${jugador.vidas} vidas`);
+console.log(jugador.perderVida());
 console.log(`El jugador ${jugador.nombre} tiene ${jugador.energia}% de energía y ${jugador.vidas} vidas`);
